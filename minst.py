77f2194fb57922dbd46582e3eb3d43ec3f6e61ca
miaochenglinx@gmail.com <miaochenglinx@gmail.com>
2017-06-22 09:17:41 +0800

    Initial commit

---
+++ minst.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+data = load_iris()
+features = data['data']
+feature_names = data['feature_names']
+target = data['target']
+
+for t, marker, c in zip(range(3), ">ox", "rgb"):
+    plt.scatter(features[target == t, 0],
+                features[target == t, 1],
+                marker=marker,
+                c=c)
+    plt.show()
+plength = features[:,2]
+is_setosa = (labels == 'setosa')
+
+
