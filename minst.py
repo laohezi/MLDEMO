e8a2f1d12e10d244d1b0c847a8243d1de12825a5
miaochenglinx@gmail.com <miaochenglinx@gmail.com>
2017-06-26 18:31:04 +0800

    日常提交

--- minst.py
+++ minst.py
@@ -2,18 +2,57 @@ from matplotlib import pyplot as plt
 from sklearn.datasets import load_iris
 import numpy as np
 
+
+
 data = load_iris()
+print("data:",data)
 features = data['data']
+
 feature_names = data['feature_names']
 target = data['target']
+labels = data['target_names']
 
-for t, marker, c in zip(range(3), ">ox", "rgb"):
-    plt.scatter(features[target == t, 0],
-                features[target == t, 1],
-                marker=marker,
-                c=c)
-    plt.show()
+# for t, marker, c in zip(range(3), ">ox", "rgb"):
+#     plt.scatter(features[target == t, 0],
+#                 features[target == t, 1],
+#                 marker=marker,
+#                 c=c)
+#     plt.show()
+
 plength = features[:,2]
+print("features:",features)
+print("plength:",plength)
 is_setosa = (labels == 'setosa')
+max_setosa = plength[is_setosa].max()
+min_non_setosa = plength[~is_setosa].min()
+print('Maximum of setosa:{0}.'.format(max_setosa))
+print('Minimum of setosa:{0}.'.format(min_non_setosa))
+
+if features[:,2] <2: print('Iris Setosa')
+
+features = features[~is_setosa]
+labels = labels[~is_setosa]
+
+virginica = (labels == 'virginica')
+
+best_acc = -1.0
+
+for fi in range(features.shape[1]):
+    print("fi:",fi)
+    thresh = features[:fi].copy()
+    thresh.sort()
+    for t in thresh:
+        pred = (features[:,fi]>t)
+        acc = (pred == virginica).mean()
+
+        if acc >best_acc:
+            best_acc = acc
+            best_fi = fi
+            best_t = t
+
+
+
+
+
 
 
