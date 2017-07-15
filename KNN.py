fd9a954a794fdc5af333a6727abc3081f6ca6d45
miaochenglinx@gmail.com <miaolaozi1469>
2017-07-16 00:51:40 +0800

    完成为女士在社交网站挑选中意男性的demo 添加手写数字的demo 第二章完成

--- KNN.py
+++ KNN.py
@@ -1,6 +1,7 @@
 from numpy import *
 import operator
 import matplotlib.pyplot as plt
+import  os
 
 
 def createDataSet():
@@ -9,17 +10,19 @@ def createDataSet():
     return group, labels
 
 
+# knn分类
 def classify0(inX, dataSet, lables, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances ** 0.5
-    sortedDistIndiacies = distances.argsort()
+    sortedDistIndiacies = distances.argsort()  # 按照距离大小的顺序排列
     classCount = {}
     for i in range(k):
         # 选取距离最小的k个点
         votelabel = lables[sortedDistIndiacies[i]]
+        # 如果该标签中标后则再对其中标次数加一，最后中标次数最多的标签则视为该测试对象的标签
         classCount[votelabel] = classCount.get(votelabel, 0) + 1
 
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
@@ -73,21 +76,74 @@ def plot():
     # s:常量或者数组 表示圆点的颜色
     ax.scatter(x=datingDataMat[:, 0], y=datingDataMat[:, 2], s=15.0 * array(dataLabels), c=15.0 * array(dataLabels))
     plt.show()
+
+
 def datingClassTest():
-    hoRatio = 0.10 #测试数据集的比率
+    hoRatio = 0.10  # 测试数据集的比率
     datingLabels = getDatingTestSet2Mat()[1]
-    normMat,ranges,minVals = autoNorm(getDatingTestSet2Mat()[0])
-    m= normMat.shape[0]
-    numTestVecs = int(m*hoRatio)
+    normMat, ranges, minVals = autoNorm(getDatingTestSet2Mat()[0])
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print("the classifier came back with: %d,the real answer is %d" %(classifierResult,datingLabels[i]))
-        if (classifierResult != datingLabels[i]):errorCount +=1.0
-    print("the total error rate is : %f"%(errorCount/float(numTestVecs)))
-
-datingClassTest()
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d,the real answer is %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("the total error rate is : %f" % (errorCount / float(numTestVecs)))
 
 
+def classifyPerson():
+    resultList = ['dislike', 'ok', 'love']
+    percentTats = float(input("玩游戏的时间百分比"))
+    ffMiles = float(input("每年的飞行距离"))
+    iceCream = float(input("吃冰淇淋的数量"))
+
+    datingMat, datingLabels = getDatingTestSet2Mat();
+    normMat, ranges, minVals = autoNorm(datingMat)
+
+    inArr = array([ffMiles, percentTats, iceCream])
+
+    result = classify0((inArr - minVals) / ranges, normMat, datingLabels, 3)
+
+    print("你会", resultList[result - 1], "这个人")
+    classifyPerson()
+
+
+def image2Vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFilelist = os.listdir("D:\文档\machinelearninginaction\Ch02\drainingDigits")
+    m = len(trainingFilelist)
+    trainingMat = zeros((m,1024))
+    for  i in range(m):
+        fileNameStr = trainingFilelist[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i:] = image2Vector('D:\文档\machinelearninginaction\Ch02\drainingDigits/%s'%fileNameStr)
+    testFileList =os.listdir('D:\文档\machinelearninginaction\Ch02\destDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = image2Vector('D:\文档\machinelearninginaction\Ch02\destDigits/%s' %fileNameStr)
+        classifierResult = classify0(vectorUnderTest,trainingMat,hwLabels,3)
+        print("the classifier come back with: %d,the real answer id :%d" %(classifierResult,classNumStr))
+        if (classifierResult != classNumStr):errorCount +=1.0
+    print("/n the total number of error is:%d"%errorCount)
+    print("/n the total error ratio is : %f" %(errorCount/float(mTest)))
+
+handwritingClassTest()
 
 
